9e875d8c0095d37a37c121ac6e211c1ad7d0dc56
Sean Wong <seanwong24@live.com>
2025-06-09 22:46:27 +0000

    refactor

--- utils/parser.lua
+++ utils/parser.lua
@@ -4,7 +4,7 @@ local M = {}
 
 local P, C, Ct, Cg, Cp, V, S = lpeg.P, lpeg.C, lpeg.Ct, lpeg.Cg, lpeg.Cp, lpeg.V, lpeg.S
 
-local function add_postions(pattern)
+local function add_positions(pattern)
 	return Cp()
 		* pattern
 		* Cp()
@@ -18,8 +18,14 @@ local function generate_command_table(command_name, named_parameters, positional
 	return { command = command_name, named_parameters = named_parameters, positional_parameters = positional_parameters }
 end
 
-local TKN = (function()
+local RULES = (function()
 	local _ = {}
+
+	setmetatable(_, {
+		__index = function(t, key)
+			return V(key)
+		end,
+	})
 
 	_.WHITESPACE = S(" \t\n\r")
 	_.BACK_SLASH = P("\\")
@@ -30,62 +36,45 @@ local TKN = (function()
 	_.PIPE = P("|")
 	_.PRESERVED = _.L_BRACE + _.R_BRACE + _.BACK_SLASH + _.L_BRACKET + _.R_BRACKET + _.PIPE
 
-	return _
-end)()
+	_.Command = add_positions(_.UserCommand + _.BracedBlock + _.Text)
 
-local grammar = P({
-	"Root",
-
-	Root = Ct(V("Block")) / function(contents)
-		return generate_command_table("__BLOCK__", nil, contents)
-	end,
-
-	Command = add_postions(V("UserCommand") + V("BracedBlock") + V("Text")),
-
-	Text = Ct(C((1 - TKN.PRESERVED) ^ 1)) / function(s)
+	_.Text = Ct(C((1 - _.PRESERVED) ^ 1)) / function(s)
 		return generate_command_table("__TEXT__", nil, s)
-	end,
+	end
 
-	UserCommand = Ct(
-		TKN.BACK_SLASH
-			* Cg(V("CommandName"), "name")
-			* Cg(Ct(V("NamedParameter") ^ 0), "named_parameters")
-			* Cg(Ct((TKN.WHITESPACE ^ 0 * V("BracedBlock")) ^ 0), "positional_parameters")
+	_.UserCommand = Ct(
+		_.BACK_SLASH
+			* Cg(_.CommandName, "name")
+			* Cg(Ct(_.NamedParameter ^ 0), "named_parameters")
+			* Cg(Ct((_.WHITESPACE ^ 0 * _.BracedBlock) ^ 0), "positional_parameters")
 	) / function(contents)
 		return generate_command_table(contents.name, contents.named_parameters, contents.positional_parameters)
-	end,
+	end
 
-	CommandName = C((1 - (TKN.WHITESPACE + TKN.PRESERVED)) ^ 1),
+	_.CommandName = C((1 - (_.WHITESPACE + _.PRESERVED)) ^ 1)
 
-	NamedParameterKeyValue = add_postions(
+	_.NamedParameterKeyValue = add_positions(
 		Ct(
-			TKN.WHITESPACE ^ 0
-				* TKN.L_BRACKET
-				* TKN.WHITESPACE ^ 0
-				* Cg(V("Command") + TKN.WHITESPACE ^ 0 / function()
+			_.WHITESPACE ^ 0
+				* _.L_BRACKET
+				* _.WHITESPACE ^ 0
+				* Cg(_.Command + _.WHITESPACE ^ 0 / function()
 					return ""
 				end, "key")
-				* TKN.WHITESPACE ^ 0
-				* TKN.PIPE
-				* TKN.WHITESPACE ^ 0
-				* Cg(V("Command") + TKN.WHITESPACE ^ 0, "value")
-				* TKN.WHITESPACE ^ 0
-				* TKN.R_BRACKET
+				* _.WHITESPACE ^ 0
+				* _.PIPE
+				* _.WHITESPACE ^ 0
+				* Cg(_.Command + _.WHITESPACE ^ 0, "value")
+				* _.WHITESPACE ^ 0
+				* _.R_BRACKET
 		)
-	),
+	)
 
-	NamedParameterFlag = add_postions(
-		Ct(
-			TKN.WHITESPACE ^ 0
-				* TKN.L_BRACKET
-				* TKN.WHITESPACE ^ 0
-				* Cg(V("Command"), "key")
-				* TKN.WHITESPACE ^ 0
-				* TKN.R_BRACKET
-		)
-	),
+	_.NamedParameterFlag = add_positions(
+		Ct(_.WHITESPACE ^ 0 * _.L_BRACKET * _.WHITESPACE ^ 0 * Cg(_.Command, "key") * _.WHITESPACE ^ 0 * _.R_BRACKET)
+	)
 
-	NamedParameter = add_postions(V("NamedParameterKeyValue") + V("NamedParameterFlag") / function(named_param)
+	_.NamedParameter = add_positions(_.NamedParameterKeyValue + _.NamedParameterFlag / function(named_param)
 		if named_param.key and named_param.value then
 			return { key = named_param.key, value = named_param.value }
 		elseif named_param.key then
@@ -93,14 +82,23 @@ local grammar = P({
 		else
 			return nil
 		end
-	end),
+	end)
 
-	BracedBlock = Ct(TKN.L_BRACE * V("Block") * TKN.R_BRACE) / function(contents)
+	_.BracedBlock = Ct(_.L_BRACE * _.Block * _.R_BRACE)
+		/ function(contents)
+			return generate_command_table("__BLOCK__", nil, contents)
+		end
+
+	_.Block = _.Command ^ 0
+
+	_[1] = Ct(_.Block) / function(contents)
 		return generate_command_table("__BLOCK__", nil, contents)
-	end,
+	end
 
-	Block = V("Command") ^ 0,
-})
+	return _
+end)()
+
+local grammar = P(RULES)
 
 local function simplify(node)
 	if type(node) ~= "table" then
